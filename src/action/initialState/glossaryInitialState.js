a8d9239c12de0da6777d38d2cea4fccf1c8e12e5
Nick Laiacona <nick@performantsoftware.com>
2023-01-16 15:16:47 -0500

    bug fix

--- src/action/initialState/glossaryInitialState.js
+++ src/action/initialState/glossaryInitialState.js
@@ -1,5 +1,5 @@
 export default {
-    commentsURL: "http://localhost:4000/bnf-ms-fr-640/local011323-1/glossary.json",  // TODO REFACTOR
+    glossaryURL: "http://localhost:4000/bnf-ms-fr-640/local011323-1/glossary.json",  // TODO REFACTOR
     glossary: {},
     loaded: false
 }	
