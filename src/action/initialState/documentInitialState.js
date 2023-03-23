6e675e02434b588977150e3cb462ddf69d787c78
Nick Laiacona <nick@performantsoftware.com>
2023-03-23 08:52:26 -0400

    add figures to new config

--- src/action/initialState/documentInitialState.js
+++ src/action/initialState/documentInitialState.js
@@ -1,7 +1,7 @@
 export default function documentInitalState(editionBaseURL) {
   return {
     currentDocumentName: 'BnF Ms. Fr. 640',
-    manifestURL: `${editionBaseURL}/manifest.json`,
+    manifestURL: `${editionBaseURL}/iiif/manifest.json`,
     folios: [],
     loaded: false,
     folioIndex: [],
