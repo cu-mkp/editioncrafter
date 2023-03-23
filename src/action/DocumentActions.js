956ec3a83a6736d170b960aecf06c3e12e621645
Nick Laiacona <nick@performantsoftware.com>
2023-03-23 15:38:44 -0400

    add support for v3 manifest from cli - wip

--- src/action/DocumentActions.js
+++ src/action/DocumentActions.js
@@ -29,27 +29,68 @@ function createFolioIndex(folios) {
   return { folioIndex, nameByID, idByName };
 }
 
+function throwError(message) {
+  throw new Error(`IIIF manifest parser error: ${message}`)
+}
+
+function displayWarning(message) {
+  console.warn(`IIIF manifest parser warning: ${message}`)
+}
+
+function parseImageURL(canvas) {
+  for( const annotationPage of canvas.items ) {
+      
+  }
+  // look for an annotation page containing an annotation with a painting motivation
+  // the ID of the body is the image API URL
+  // add info.json to it
+}
+
+function parseLabel(canvas) {
+  // TODO support labels in different languages, multiple labels in same language
+  if( !canvas.label ) {
+    displayWarning(`${canvas.id} does not have a label property.`);
+    return "";
+  }
+  return canvas.label.none[0]
+}
+
+function parseThumbnailURL(canvas) {
+  // TODO 
+  // `${canvas.thumbnail['@id']}/full/native.jpg`;
+}
+
+function parseAnnotationURLs(canvas) {
+  // TODO
+}
+
 function parseManifest(manifest) {
   const folios = [];
-  const { canvases } = manifest.sequences[0];
 
-  for (const canvas of canvases) {
-    const canvasID = canvas['@id'];
-    const canvasLabel = canvas.label;
-    const imageURL = `${canvas.images[0].resource.service['@id']}/info.json`;
-    const thumbnailURL = `${canvas.thumbnail['@id']}/full/native.jpg`;
-    let annotationListURL = null;
+  // make sure this is a IIIF Presentation API v3 Manifest 
+  if( !manifest['@context'] || manifest['@context'].includes("http://iiif.io/api/presentation/3/context.json") ) {
+    throwError("Expected root object to have a @context containing: http://iiif.io/api/presentation/3/context.json");
+  }
 
-    if (canvas.otherContent) {
-      annotationListURL = canvas.otherContent[0]['@id'];
-    }
+  if( manifest.type !== "Manifest" ) throwError("Expected root object of type 'Manifest'.");
+  if( !manifest.items ) throwError("Expected manifest to have an items property.");
+  const canvases = manifest.items;
+
+  for ( let i=0; i < canvases.length; i++) {
+    const canvas = canvases[i]
+    if( canvas.type !== "Canvas" ) throwError(`Expected items[${i}] to be of type 'Canvas'.`);
+    if( !canvas.id ) throwError(`Expected items[${i}] to have an id property.`);
+    const canvasLabel = parseLabel(canvas);
+    const imageURL = parseImageURL(canvas);
+    const thumbnailURL = parseThumbnailURL(canvas);
+    const annotationURLs = parseAnnotationURLs(canvas);
 
     const folio = new Folio({
-      id: canvasID,
+      id: canvas.id,
       name: canvasLabel,
       image_zoom_url: imageURL,
       image_thumbnail_url: thumbnailURL,
-      annotationListURL,
+      annotationURLs,
     });
 
     folios.push(folio);
