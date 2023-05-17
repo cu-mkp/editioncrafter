c5de8912e8c4092a7f5c4c6f20e8daa9d37198f4
Camden Mecklem <c@camdenmecklem.com>
2023-05-17 09:02:49 -0400

    fix help popup CSS (#41)

--- editioncrafter/src/component/HelpPopper.js
+++ editioncrafter/src/component/HelpPopper.js
@@ -8,8 +8,13 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import {
   FaArrowCircleLeft,
-  FaArrowCircleRight
+  FaArrowCircleRight,
 } from 'react-icons/fa';
+import asterisk from '../icons/howtouse-asterisk.png';
+import curly from '../icons/howtouse-curly.png';
+import beaker from '../icons/howtouse-beaker.png';
+import square from '../icons/howtouse-square.png';
+import ups from '../icons/howtouse-ups.png';
 
 const HelpPopper = (props) => (
   <Popper anchorEl={props.anchorEl} open={props.open} style={props.marginStyle}>
@@ -22,26 +27,26 @@ const HelpPopper = (props) => (
           <Typography variant="h6" component="h2">Toolbar Buttons</Typography>
           <List>
             <ListItem>
-              <span className="fa fa-lock active" />
+              <span className="fa fa-lock" />
               <ListItemText primary="Toggle Sync Views" />
             </ListItem>
             <ListItem>
-              <span className="fa fa-book active" />
+              <span className="fa fa-book" />
               <ListItemText primary="Toggle Book Mode" />
             </ListItem>
             <ListItem>
-              <span className="fa fa-code active" />
+              <span className="fa fa-code" />
               <ListItemText primary="Toggle XML Mode" />
             </ListItem>
             <ListItem>
-              <span>
+              <span className="horizontal-buttons">
                 <FaArrowCircleLeft />
                 <FaArrowCircleRight />
               </span>
               <ListItemText primary="Go Forward / Back" />
             </ListItem>
             <ListItem>
-              <span className="fa fa-hand-point-right active" />
+              <span className="fa fa-hand-point-right" />
               <ListItemText primary="Jump to folio" />
             </ListItem>
             <div className="readingGuide">
@@ -55,23 +60,23 @@ const HelpPopper = (props) => (
                 </thead>
                 <tbody>
                   <tr>
-                    <td><img src="/img/howtouse-ups.png" alt="howtouse-ups" /></td>
+                    <td><img src={ups} alt="howtouse-ups" /></td>
                     <td><Typography>Text is under the paper strip</Typography></td>
                   </tr>
                   <tr>
-                    <td><img src="/img/howtouse-curly.png" alt="howtouse-curly" /></td>
+                    <td><img src={curly} alt="howtouse-curly" /></td>
                     <td><Typography>Expanded version implied by abbreviation marks</Typography></td>
                   </tr>
                   <tr>
-                    <td><img src="/img/howtouse-square.png" alt="howtouse-square" /></td>
+                    <td><img src={square} alt="howtouse-square" /></td>
                     <td><Typography>Editorial interventions and corrections</Typography></td>
                   </tr>
                   <tr>
-                    <td><img src="/img/howtouse-beaker.png" alt="howtouse-beaker" /></td>
+                    <td><img src={beaker} alt="howtouse-beaker" /></td>
                     <td><Typography>Associated research essay</Typography></td>
                   </tr>
                   <tr>
-                    <td><img src="/img/howtouse-asterisk.png" alt="howtouse-asterisk" /></td>
+                    <td><img src={asterisk} alt="howtouse-asterisk" /></td>
                     <td><Typography>Editorial Comments</Typography></td>
                   </tr>
                 </tbody>
@@ -80,8 +85,9 @@ const HelpPopper = (props) => (
           </List>
           <Typography>
             See
+            &nbsp;
             <a href="#/content/how-to-use">How to Use</a>
-            {' '}
+            &nbsp;
             for more information.
           </Typography>
         </div>
