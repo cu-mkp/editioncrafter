0a52d73682e794ef6c9758d1ee8c3f0244672daf
Camden Mecklem <c@camdenmecklem.com>
2023-05-19 15:42:27 -0400

    remove research essay features

--- editioncrafter/src/model/DocumentHelper.js
+++ editioncrafter/src/model/DocumentHelper.js
@@ -1,12 +1,11 @@
 const DocumentHelper = {};
 
 DocumentHelper.transcriptionTypeLabels = {
-  tc: 'Diplomatic (FR)',
-  tcn: 'Normalized (FR)',
-  tl: 'Translation (EN)',
-  f: 'Facsimile',
-  anno: 'Research Esssay',
-  glossary: 'Glossary',
+  tc: 'Diplomatic (FR)', // configurable
+  tcn: 'Normalized (FR)', // configurable
+  tl: 'Translation (EN)', // configurable
+  f: 'Facsimile', // keep
+  glossary: 'Glossary', // keep
 };
 
 export default DocumentHelper;
