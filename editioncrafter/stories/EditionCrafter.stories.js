090ef78fe2c8d189a66741f7dabadccda84764bf
Camden Mecklem <c@camdenmecklem.com>
2023-06-01 12:40:22 -0400

    add storybook pipeline

--- editioncrafter/stories/EditionCrafter.stories.js
+++ editioncrafter/stories/EditionCrafter.stories.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import EditionCrafter from '../src/index';
 
-const config = {
-  iiifManifest: 'http://localhost:8080/fr640_3r-3v-example/iiif/manifest.json',
+const baseConfig = {
   documentName: 'BnF Ms. Fr. 640',
   transcriptionTypes: {
     tc: 'Diplomatic (FR)',
     tcn: 'Normalized (FR)',
     tl: 'Translation (EN)',
   },
-  commentsURL: '/fr640_3r-3v-example/comments.json',
-  glossaryURL: '/fr640_3r-3v-example/glossary.json',
 };
 
-export const Default = () => (
-  <EditionCrafter config={config} />
+export const Development = () => (
+  <EditionCrafter config={{
+    ...baseConfig,
+    iiifManifest: 'http://localhost:8080/fr640_3r-3v-example/iiif/manifest.json',
+    commentsURL: '/fr640_3r-3v-example/comments.json',
+    glossaryURL: '/fr640_3r-3v-example/glossary.json',
+  }}
+  />
+);
+
+export const Testing = () => (
+  <EditionCrafter config={{
+    ...baseConfig,
+    iiifManifest: 'https://cu-mkp.github.io/editioncrafter-project/fr640_3r-3v-example/iiif/manifest.json',
+    commentsURL: 'https://cu-mkp.github.io/editioncrafter-project/fr640_3r-3v-example/comments.json',
+    glossaryURL: 'https://cu-mkp.github.io/editioncrafter-project/fr640_3r-3v-example/glossary.json',
+  }}
+  />
 );
 
 export default {
