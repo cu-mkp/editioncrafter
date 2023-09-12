bd97f17cad141fe7e569071656d6958ac7c15bea
Camden Mecklem <c@camdenmecklem.com>
2023-09-12 13:05:02 -0400

    fix crash on transcriptions with ref attributes (#58)

---
+++ editioncrafter/src/component/Parser.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import HtmlReactParser from 'html-react-parser';
+
+// This component addresses a conflict between React and
+// documents whose elements contain `ref` attributes.
+// Because we're parsing transcription contents as React
+// elements, migrating TranscriptionView to a function
+// component caused React to error about the ref attributes.
+
+// The short-term fix is to wrap this simple class component
+// around the parser.
+
+class Parser extends React.Component {
+  render() {
+    const { html, htmlToReactParserOptionsSide } = this.props;
+
+    return HtmlReactParser(html, htmlToReactParserOptionsSide);
+  }
+}
+
+export default Parser;
