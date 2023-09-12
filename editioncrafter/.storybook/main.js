ea505456a04a55ced68aeb8b04bd33ea1cbbe99c
Camden Mecklem <c@camdenmecklem.com>
2023-09-12 16:31:41 -0400

    replace storybook styling addon

--- editioncrafter/.storybook/main.js
+++ editioncrafter/.storybook/main.js
@@ -5,15 +5,50 @@ const config = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    {
-      name: '@storybook/addon-styling',
-      options: {
-        sass: {
-          implementation: require('sass'),
-        },
-      },
-    },
-    "@storybook/addon-styling-webpack"
+    "@storybook/addon-styling-webpack",
+    ({
+        name: "@storybook/addon-styling-webpack",
+
+        options: {
+          rules: [{
+        test: /\.css$/,
+        sideEffects: true,
+        use: [
+            require.resolve("style-loader"),
+            {
+                loader: require.resolve("css-loader"),
+                options: {
+
+
+                },
+            },
+        ],
+      },{
+        test: /\.s[ac]ss$/,
+        sideEffects: true,
+        use: [
+            require.resolve("style-loader"),
+            {
+                loader: require.resolve("css-loader"),
+                options: {
+
+                    importLoaders: 2,
+                },
+            },
+            require.resolve("resolve-url-loader"),
+            {
+                loader: require.resolve("sass-loader"),
+                options: {
+                    // Want to add more Sass options? Read more here: https://webpack.js.org/loaders/sass-loader/#options
+                    implementation: require.resolve("sass"),
+                    sourceMap: true,
+                    sassOptions: {},
+                },
+            },
+        ],
+      },],
+        }
+    })
   ],
   framework: {
     name: "@storybook/react-webpack5",
