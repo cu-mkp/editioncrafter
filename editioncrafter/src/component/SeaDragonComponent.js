a72f948c61edc4519743011a66d2be728ce1c672
Rebecca Black <rebecca@performantsoftware.com>
2024-03-05 14:14:37 -0500

    Adding astro frontend site (#1)
    * trying width and height auto
    
    * resize based on container
    
    * add loading spinner component
    
    * added loading spinner to image and transcription views
    
    * made dropdown and help icon visible on single pane
    
    * initial creation of astro site folder
    
    * built out index page
    
    * added buttons
    
    * updated header
    
    * updated deploy.yml to pull from main
    
    * Create deploy.yml
    
    * Update astro.config.mjs

--- editioncrafter/src/component/SeaDragonComponent.js
+++ editioncrafter/src/component/SeaDragonComponent.js
@@ -1,11 +1,15 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useState } from 'react';
+import { BigRingSpinner } from './RingSpinner';
 
 const SeaDragonComponent = (props) => {
   const { side, initViewer, tileSource } = props;
+  const [loading, setLoading] = useState(true);
 
   const viewer = useMemo(() => (
-    <div id={`image-view-seadragon-${side}`} ref={(el) => { initViewer(el, tileSource); }} />
-  ), []);
+    <div id={`image-view-seadragon-${side}`} ref={(el) => { initViewer(el, tileSource).then(() => {setLoading(false);}) }}>
+      { loading && <BigRingSpinner color="light" />}
+    </div>
+  ), [loading]);
 
   return viewer;
 };
