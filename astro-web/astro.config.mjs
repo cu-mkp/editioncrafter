7b61d09be04de8893d5b840dbfad58e7f812c890
Rebecca Black <rebecca.ann.black@gmail.com>
2024-03-05 04:49:54 -0500

    changed base in astro config

--- astro-web/astro.config.mjs
+++ astro-web/astro.config.mjs
@@ -7,5 +7,5 @@ import react from "@astrojs/react";
 export default defineConfig({
   integrations: [tailwind(), react()],
   site: 'https://performant-software.github.io',
-  base: '/editioncrafter/astro-web'
+  base: '/editioncrafter'
 });
