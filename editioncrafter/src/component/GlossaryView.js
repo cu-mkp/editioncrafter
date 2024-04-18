f781c50791cab185e8fe419dfa1dbd37273dd7a3
Rebecca Black <rebecca.ann.black@gmail.com>
2024-04-18 14:51:09 -0400

    updated glossary to use metadata (citation, title)

--- editioncrafter/src/component/GlossaryView.js
+++ editioncrafter/src/component/GlossaryView.js
@@ -4,6 +4,7 @@ import { Typography } from '@material-ui/core';
 import Parser from 'html-react-parser';
 import Navigation from './Navigation';
 import { alpha } from './AlphabetLinks';
+import Markdown from 'react-markdown';
 
 class GlossaryView extends Component {
   constructor() {
@@ -14,7 +15,7 @@ class GlossaryView extends Component {
   renderGlossary() {
     const { glossary } = this.props.glossary;
     const filterTerm = this.state.filterTerm.toLowerCase();
-    const entryList = Object.values(glossary);
+    const entryList = Object.values(glossary.entries);
 
     const checkHeadwords = (headword, term, delimiter) => {
       const words = headword.split(delimiter);
@@ -107,34 +108,24 @@ class GlossaryView extends Component {
           <div id="glossaryContent">
             <Typography variant="h2" className="title">Glossary</Typography>
             {
-              this.props.glossary.glossary_metadata && this.props.glossary.glossary_metadata.subtitle ? (
-                <Typography>
-                  { this.props.glossary.gossary_metadata.subtitle }
+              this.props.glossary.title && 
+                <Typography className="subtitle">
+                  <Markdown>
+                    { this.props.glossary.title }
+                  </Markdown>
                 </Typography>
-            ) : (
-            <Typography className="subtitle">
-              For short titles, e.g., [COT1611], see
-              <a href="#/content/resources/bibliography">Bibliography</a>
-              .
-            </Typography>
-            )
           }
             <div className="cite-instructions">
-              <Typography className="cite-header">How to Cite</Typography>
               {
-                this.props.glossary.glossary_metadata && this.props.glossary.glossary_metadata.citation ? (
+                this.props.glossary.citation &&
+                <>
+                  <Typography className="cite-header">How to Cite</Typography>
                   <Typography>
-                    { this.props.glossary.glossary_metadata.citation }
+                    <Markdown>
+                      { this.props.glossary.citation }
+                    </Markdown>
                   </Typography>
-                ) : (
-                  <Typography>
-                    “Glossary.” In
-                    <i>Secrets of Craft and Nature in Renaissance France. A Digital Critical Edition and English Translation of BnF Ms. Fr. 640</i>
-                    , edited by Making and Knowing Project, Pamela H. Smith, Naomi Rosenkranz, Tianna Helena Uchacz, Tillmann Taape, Clément Godbarge, Sophie Pitman, Jenny Boulboullé, Joel Klein, Donna Bilak, Marc Smith, and Terry Catapano. New York: Making and Knowing Project, 2020.
-                    <a href="https://edition640.makingandknowing.org/#/folios/1r/f/1r/glossary">https://edition640.makingandknowing.org/#/folios/1r/f/1r/glossary</a>
-                    .
-                  </Typography>
-                )
+                </>
               }
             </div>
             { this.renderGlossary() }
