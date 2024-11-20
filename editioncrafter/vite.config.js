06e29c69d8f9ebc7f79bb055ba7a003702fcc31c
Camden Mecklem <c@camdenmecklem.com>
2024-11-20 10:46:06 -0500

    finish Vite config

--- editioncrafter/vite.config.js
+++ editioncrafter/vite.config.js
@@ -1,11 +1,13 @@
 import { resolve } from 'node:path'
 import react from '@vitejs/plugin-react'
 import { defineConfig, transformWithEsbuild } from 'vite'
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    cssInjectedByJsPlugin(),
     {
       name: 'treat-js-files-as-jsx',
       async transform(code, id) {
@@ -27,21 +29,25 @@ export default defineConfig({
       entry: resolve(__dirname, 'src/index.js'),
       name: 'EditionCrafter',
       fileName: 'editioncrafter',
+      formats: ['es'],
     },
+    assetsInlineLimit: 999999999999,
     rollupOptions: {
       external: ['react', 'react-dom'],
       output: {
         globals: {
-          react: 'React',
-          'react-dom': "ReactDOM"
-        }
-      }
-    }
+          'react': 'React',
+          'react-dom': 'ReactDOM',
+        },
+        manualChunks: undefined,
+      },
+    },
   },
   esbuild: {
     loader: 'jsx',
     include: /.*\.jsx?$/,
     exclude: [],
+
   },
   optimizeDeps: {
     esbuildOptions: {
