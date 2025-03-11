a3c42c0400f5aa4f7c2ff59ccc47b5cb3c84ee2b
Nick Laiacona <nick@performantsoftware.com>
2025-03-11 09:13:06 -0400

    fix height

--- editioncrafter/src/TagExplore/components/NarrowSidebar.jsx
+++ editioncrafter/src/TagExplore/components/NarrowSidebar.jsx
@@ -8,25 +8,27 @@ import { List, ListItem, ListItemIcon } from '@material-ui/core'
 function NarrowSidebar(props) {
     const { helpRef, toggleDrawer, toggleHelp } = props
     return (
-        <List>
-            <ListItem 
-                onClick={toggleDrawer}
-                button
-            >
-                <ListItemIcon>
-                    <BsFillGrid3X3GapFill />
-                </ListItemIcon>
-            </ListItem>
-            <ListItem
-                onClick={toggleHelp}
-                ref={helpRef}
-                button
-            >
-                <ListItemIcon>
-                    <FaQuestionCircle />
-                </ListItemIcon>
-            </ListItem>
-        </List>
+        <div className="narrow-sidebar">
+            <List>
+                <ListItem 
+                    onClick={toggleDrawer}
+                    button
+                >
+                    <ListItemIcon>
+                        <BsFillGrid3X3GapFill />
+                    </ListItemIcon>
+                </ListItem>
+                <ListItem
+                    onClick={toggleHelp}
+                    ref={helpRef}
+                    button
+                >
+                    <ListItemIcon>
+                        <FaQuestionCircle />
+                    </ListItemIcon>
+                </ListItem>
+            </List>
+        </div>
     )
 }
 
