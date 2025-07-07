6f74211c9c2830da935bcbd76e0cc39279da4ca1
Rebecca Black <rebecca.ann.black@gmail.com>
2025-07-06 23:00:40 -0400

    updates to tag explorer

--- editioncrafter/src/TagExplore/components/SurfaceBrowser.jsx
+++ editioncrafter/src/TagExplore/components/SurfaceBrowser.jsx
@@ -4,24 +4,40 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'
 import GridOnIcon from '@material-ui/icons/GridOn'
 import ListIcon from '@material-ui/icons/List'
 import TuneIcon from '@material-ui/icons/Tune'
-import { useEffect, useMemo, useState } from 'react'
+import { useCallback, useEffect, useMemo, useState } from 'react'
 
 import { useLocation, useNavigate } from 'react-router-dom'
 import { getObjs } from '../../common/lib/sql'
 import DocumentDetail from './DocumentDetail'
+// import DocumentFilters from './DocumentFilters'
 import TagFilters from './TagFilters'
 
 function getData(db) {
   const docStmt = db.prepare(`
       SELECT
-        documents.id AS id,
-        documents.name AS name,
-        documents.local_id AS local_id
+        d.id AS id,
+        d.name AS name,
+        d.local_id AS local_id,
+        json_group_array(document_taggings.tag) as tags
       FROM
-        documents
+        documents d
+          LEFT JOIN document_taggings ON d.id = document_taggings.document
+      GROUP BY d.id, d.name, d.local_id
     `)
-
   return getObjs(docStmt)
+}
+
+function getDocTags(db) {
+  const tagsStmt = db.prepare(`
+    SELECT
+      tags.id AS id
+    FROM
+      tags
+        LEFT JOIN taxonomies ON tags.taxonomy_id = taxonomies.id
+    WHERE NOT
+      taxonomies.is_surface = 1
+    `)
+  return getObjs(tagsStmt)
 }
 
 function parseFolioID(folioID) {
@@ -50,14 +66,31 @@ function getSelection(path) {
 function SurfaceBrowser(props) {
   const { db, open, toggleOpen } = props
   const documents = useMemo(() => getData(db), [db])
+  const docTags = useMemo(() => getDocTags(db), [db])
   const [pageCount, setPageCount] = useState({})
   const [totalPages, setTotalPages] = useState(0)
   const [tags, setTags] = useState([])
   const [showFilters, setShowFilters] = useState(false)
+  const [query, setQuery] = useState(null)
+
+  const filterDocs = useCallback((docs, tags) => {
+    return docs.filter((doc) => {
+      for (const tag of tags) {
+        if (docTags.includes(tag) && !JSON.parse(doc.tags)?.includes(tag)) {
+          return false
+        }
+      }
+      return true
+    })
+  }, [docTags])
+
+  const filteredDocs = useMemo(() => (filterDocs(documents, tags)), [filterDocs, documents, tags])
 
   const navigate = useNavigate()
   const location = useLocation()
   const selection = useMemo(() => getSelection(location.pathname), [location])
+
+  const numFilters = useMemo(() => (tags.length), [tags])
 
   const navigateToSelection = (nextSelection) => {
     const folioID = nextSelection?.left ? `${nextSelection.left.localID}_${nextSelection.left.surfaceID}` : null
@@ -152,18 +185,22 @@ function SurfaceBrowser(props) {
           </IconButton>
         </ButtonGroup> */}
         { showFilters && (
-          <TagFilters
-            db={db}
-            filters={tags}
-            onToggleSelected={(tagId) => {
-              if (tags.includes(tagId)) {
-                setTags(current => (current.filter(t => (t !== tagId))))
-              }
-              else {
-                setTags(current => ([...current, tagId]))
-              }
-            }}
-          />
+          <div className="tag-filters">
+            <Input placeholder="Search for filters" value={query} onChange={(e) => { setQuery(e.target.value) }} className="tag-filters-search" />
+            <TagFilters
+              db={db}
+              filters={tags}
+              query={query}
+              onToggleSelected={(tagId) => {
+                if (tags.includes(tagId)) {
+                  setTags(current => (current.filter(t => (t !== tagId))))
+                }
+                else {
+                  setTags(current => ([...current, tagId]))
+                }
+              }}
+            />
+          </div>
         ) }
         <Box className="surface-browser-document-details">
           { documentDetails }
